ac4e07a4d72a05c2633c5c5ea67354d66ddb6ac5
poddarashit <55320394+poddarashit@users.noreply.github.com>
2020-03-18 21:04:05 -0300

    adding two programming challenges

---
+++ Jackpot/Jackpot/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Jackpot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //defining dictionary variable
+
+            Dictionary<int, int> groupA = new Dictionary<int, int>();
+            Dictionary<int, int> groupB = new Dictionary<int, int>();
+
+            // adding key value pairs to dictionary
+
+            groupA.Add(1, 3000);
+            groupA.Add(2, 5000);
+            groupA.Add(3, 7000);
+            groupA.Add(4, 10000);
+
+            groupB.Add(1, 2000);
+            groupB.Add(2, 3000);
+            groupB.Add(3, 4000);
+            groupB.Add(4, 5000);
+
+            int jackpotValue = 0;
+            bool isNumber = false;
+
+            while (!isNumber)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Jackpot Value:");
+                    jackpotValue = Convert.ToInt32(Console.ReadLine());
+                    isNumber = true;
+
+                    //object creation
+                    var prog = new Program();
+
+                    //function calling
+                    prog.findWinningPairs(groupA, groupB, jackpotValue);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Please enter valid number.");
+                }
+
+            }
+                  
+        }
+
+        public void findWinningPairs(Dictionary<int, int> groupA, Dictionary<int, int> groupB, int jackpotValue)
+        {
+            bool jackpotFlag = false;
+
+            try
+            {
+                foreach (KeyValuePair<int, int> dicA in groupA)
+                {
+                    foreach (KeyValuePair<int, int> dicB in groupB)
+                    {
+                        if (dicA.Value + dicB.Value == jackpotValue)
+                        {
+                            jackpotFlag = true;
+                            Console.WriteLine(dicA.Key.ToString() + "," + dicB.Key.ToString());
+                        }
+                    }
+                }
+
+                if (!jackpotFlag)
+                {
+                    Console.WriteLine("No Winning pair");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ distanceBetweenTwoGPSCoordinates/distanceBetweenTwoGPSCoordinates/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Data;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
+namespace distanceBetweenTwoGPSCoordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Google maps api key
+                string api_key = <your key here>;
+
+                //take user inputs for latitude and longitude for origin and destination
+                Console.WriteLine("Enter latitude for origin");
+                double lat_origin = ValidateLat(Console.ReadLine());
+                Console.WriteLine("Enter longitude for origin");
+                double lon_origin = ValidateLon(Console.ReadLine());
+                Console.WriteLine("Enter latitude for destination");
+                double lat_destination = ValidateLat(Console.ReadLine());
+                Console.WriteLine("Enter longitude for destination");
+                double lon_destination = ValidateLon(Console.ReadLine());
+
+                //call to api
+                string url = "https://maps.googleapis.com/maps/api/distancematrix/json?&origins=" + lat_origin + "," + lon_origin + "&destinations=" + lat_destination + "," + lon_destination + "&key=" + api_key;
+                WebRequest request = WebRequest.Create(url);
+                using (WebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        string result = reader.ReadToEnd();
+
+                        //parsing json data to get distance in km
+                        JObject joResponse = JObject.Parse(result);
+                        JArray jArr = (JArray)joResponse["rows"];
+                        JObject elemObj = (JObject)jArr[0];
+                        JArray elmArr = (JArray)elemObj["elements"];
+                        JObject distObj = (JObject)elmArr[0];
+                        JObject dist = (JObject)distObj["distance"];
+
+                        Console.WriteLine("Distance between the two locations is: " + dist["text"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //validate latitude values
+        public static double ValidateLat(string latitude)
+        {
+            double lat1;
+            if (Double.TryParse(latitude, out lat1))
+            {
+                if (lat1 < -90 || lat1 > 90)
+                {
+                    throw new ArgumentOutOfRangeException("Latitude must be between -90 and 90 degrees inclusive.");
+                }
+            }
+            return lat1;
+        }
+
+        //validate longitude values
+        public static double ValidateLon(string longitude)
+        {
+            double lon1;
+            if (Double.TryParse(longitude, out lon1))
+            {
+                if (lon1 < -180 || lon1 > 180)
+                {
+                    throw new ArgumentOutOfRangeException("Longitude must be between -180 and 180 degrees inclusive.");
+                }
+            }
+            return lon1;
+        }
+
+    }
+}
